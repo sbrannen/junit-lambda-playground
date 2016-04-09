fe745d23e2c2ae935501f10a7ec47cc2222f2ef2
Sam Brannen <sam@sambrannen.com>
2016-04-09 18:18:11 +0200

    Upgrade to Gradle 2.12, use JUnit 4.12, copyright headers, etc.

--- src/test/java/org/junit/lambda/engine/junit5/JavaTestDescriptorTests.java
+++ src/test/java/org/junit/lambda/engine/junit5/JavaTestDescriptorTests.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 
 package org.junit.lambda.engine.junit5;
 
@@ -7,6 +16,8 @@ import static org.junit.lambda.core.TestDescriptor.Type.ROOT;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 
+import org.junit.Test;
+
 /**
  * Unit tests for {@link JavaTestDescriptor}.
  *
@@ -15,20 +26,21 @@ import java.math.BigDecimal;
  */
 public class JavaTestDescriptorTests {
 
-	public static void main(String... args) throws Exception {
+	@Test
+	public void methodDescriptor() throws Exception {
 
 		Class<JavaTestDescriptorTests> testClass = JavaTestDescriptorTests.class;
 		Method testMethod = testClass.getDeclaredMethod("test");
 		JavaTestDescriptor descriptor = new JavaTestDescriptor(testClass, testMethod, ROOT, false, null, null);
 
-		System.err.println(descriptor);
-		System.err.println(descriptor.getId());
+		// System.err.println(descriptor);
+		// System.err.println(descriptor.getId());
 
 		testMethod = testClass.getDeclaredMethod("test", String.class, BigDecimal.class);
 		descriptor = new JavaTestDescriptor(testClass, testMethod, ROOT, false, null, null);
 
-		System.err.println(descriptor);
-		System.err.println(descriptor.getId());
+		// System.err.println(descriptor);
+		// System.err.println(descriptor.getId());
 
 		assertEquals(
 			"junit5:org.junit.lambda.engine.junit5.JavaTestDescriptorTests#test(java.lang.String, java.math.BigDecimal)",
